300f6f3abb49dba6872d75b71ba3cb0d26f14bf2
SamruddhiGujar <112796541+SamruddhiGujar@users.noreply.github.com>
2026-01-21 23:48:36 +0530

    Adding new file

---
+++ CheckAddress.java
@@ -0,0 +1,9 @@
+package com.Exponent.UserManagment.Utility;
+
+public class CheckAddress extends RuntimeException {
+	
+       public CheckAddress(String msg){
+    	   super(msg);
+    	   
+       }
+}
